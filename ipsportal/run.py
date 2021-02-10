c8789b89bf266214030da4bc8b5b5bd5307f9d7a
Ross Whitfield <whitfieldre@ornl.gov>
2021-02-10 13:21:18 -0500

    Add POST API
    e.g. curl -X POST -H "Content-Type: application/json" -d '{"code": "Framewddork", "eventtype": "IPS_END", "ok": true, "comment": "Simulation Ended", "walltime": "0.09", "state": "Completed", "stopat": "2021-02-09|14:15:53EST", "sim_name": "test", "phystimestamp": 999, "portal_runid": "pc0117540_USER_2021-02-09T14:15:53.282", "seqnum": 2}' 127.0.0.1:5000

--- ipsportal/run.py
+++ ipsportal/run.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+import json
+from flask import Blueprint, render_template, request, jsonify
 from ipsportal.db import get_db
 
 bp = Blueprint('index', __name__)
@@ -10,12 +11,38 @@ def index():
     runs = db.execute("SELECT * FROM run").fetchall()
     return render_template("index.html", runs=runs)
 
-@bp.route("/run/<int:id>")
+
+@bp.route("/<int:id>")
 def run(id):
     db = get_db()
-    run = db.execute('SELECT * FROM run WHERE id=?', str(id)).fetchone()
-    if run is None:
+    r = db.execute('SELECT * FROM run WHERE id=?', str(id)).fetchone()
+    if r is None:
         return "Run id {0} doesn't exist.".format(id)
-    portal_runid = run['portal_runid']
+    portal_runid = r['portal_runid']
     events = db.execute("SELECT * FROM event WHERE portal_runid=?", (portal_runid,)).fetchall()
     return render_template("events.html", events=events)
+
+
+@bp.route("/", methods=("POST", "GET"))
+def event():
+    e = json.loads(request.data)
+    db = get_db()
+    if e.get('eventtype') == "IPS_START":
+        db.execute("INSERT INTO run (portal_runid, state, rcomment, tokamak, shotno) VALUES (?,?,?,?,?)",
+                   (e.get('portal_runid'),
+                    e.get('state'),
+                    e.get('rcomment'),
+                    e.get('tokamak'),
+                    e.get('shotno')))
+
+    db.execute("INSERT INTO event (code, eventtype, comment, walltime, phystimestamp, portal_runid, seqnum) VALUES (?,?,?,?,?,?,?)",
+               (e.get('code'),
+                e.get('eventtype'),
+                e.get('comment'),
+                e.get('walltime'),
+                e.get('phystimestamp'),
+                e.get('portal_runid'),
+                e.get('seqnum')))
+    db.commit()
+
+    return jsonify(e)
