800968cb770777e0feab9a2bdfb8f96f84c56f13
Ross Whitfield <whitfieldre@ornl.gov>
2021-03-03 13:46:44 -0500

    sqlite -> mongo

--- ipsportal/run.py
+++ ipsportal/run.py
@@ -1,4 +1,6 @@
 import json
+import time
+import pymongo
 from flask import Blueprint, render_template, request
 from ipsportal.db import get_db
 
@@ -8,53 +10,40 @@ bp = Blueprint('index', __name__)
 @bp.route("/")
 def index():
     db = get_db()
-    runs = db.execute("SELECT * FROM run ORDER BY id DESC").fetchall()
+    runs = db.run.find().sort('id', pymongo.DESCENDING)
     return render_template("index.html", runs=runs)
 
 
 @bp.route("/<int:id>")
 def run(id):
     db = get_db()
-    r = db.execute('SELECT * FROM run WHERE id=?', (str(id),)).fetchone()
+    r = db.run.find_one({'id': id})
     if r is None:
-        return ("Run ID {0} does not exist.".format(id), 404)
-    portal_runid = r['portal_runid']
-    events = db.execute("SELECT * FROM event WHERE portal_runid=? ORDER BY seqnum DESC", (portal_runid,)).fetchall()
+        return render_template("notfound.html", run=id)
+
+    events = db.event.find({'portal_runid': r['portal_runid']}).sort('seqnum', pymongo.DESCENDING)
+
     return render_template("events.html", run=r, events=events)
 
 
 @bp.route("/", methods=("POST", "GET"))
 def event():
     e = json.loads(request.data)
+
     required = {'code', 'eventtype', 'comment', 'walltime', 'phystimestamp', 'portal_runid', 'seqnum'}
     if not e.keys() >= required:
         return ('failed', 400)
+
+    e['created'] = time.strftime('%Y-%m-%d|%H:%M:%S%Z', time.localtime())
+
     db = get_db()
+
     if e.get('eventtype') == "IPS_START":
-        db.execute("INSERT INTO run (portal_runid, state, rcomment, tokamak, shotno, simname, host, user, startat, simrunid, outputprefix, tag, simroot) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                   (e.get('portal_runid'),
-                    e.get('state'),
-                    e.get('rcomment'),
-                    e.get('tokamak'),
-                    e.get('shotno'),
-                    e.get('simname'),
-                    e.get('host'),
-                    e.get('user'),
-                    e.get('startat'),
-                    e.get('simrunid'),
-                    e.get('outputprefix'),
-                    e.get('tag'),
-                    e.get('simroot')))
+        e['id'] = db.run.estimated_document_count()
+        db.run.insert_one(e)
     elif e.get('eventtype') == "IPS_END":
-        db.execute("UPDATE run SET state = ?, stopat = ? WHERE portal_runid = ?", (e.get('state'), e.get('stopat'), e.get('portal_runid')))
-    db.execute("INSERT INTO event (code, eventtype, comment, walltime, phystimestamp, portal_runid, seqnum) VALUES (?,?,?,?,?,?,?)",
-               (e.get('code'),
-                e.get('eventtype'),
-                e.get('comment'),
-                e.get('walltime'),
-                e.get('phystimestamp'),
-                e.get('portal_runid'),
-                e.get('seqnum')))
-    db.commit()
+        db.run.update_one({'portal_runid': e.get('portal_runid')}, {'$set': e})
+
+    db.event.insert_one(e)
 
     return ('success', 200)
