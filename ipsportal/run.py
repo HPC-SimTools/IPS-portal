4f4d490e33d44652bc478c7baa42dc7cc8cdb161
Ross Whitfield <whitfieldre@ornl.gov>
2021-03-04 13:47:50 -0500

    Add ?rows= &sort= & direction=

--- ipsportal/run.py
+++ ipsportal/run.py
@@ -8,16 +8,33 @@ from ipsportal.db import get_db
 bp = Blueprint('index', __name__)
 
 ROWS_PER_PAGE = 20
+SORT_BY_DEFAULT = 'runid'
+SORT_DIRECTION_DEFAULT = -1
 
 
 @bp.route("/")
 def index():
     db = get_db()
-    page = request.args.get('page', 1, type=int)
-    num_pages = math.ceil(db.run.estimated_document_count() / ROWS_PER_PAGE)
-    pages = range(1, num_pages+1)
-    runs = db.run.find(skip=(page-1)*ROWS_PER_PAGE, limit=ROWS_PER_PAGE).sort('runid', pymongo.DESCENDING)
-    return render_template("index.html", runs=runs, pages=pages, page=page, num_pages=num_pages)
+    page = {}
+    page['page'] = request.args.get('page', 1, type=int)
+    page['rows'] = request.args.get('rows', ROWS_PER_PAGE, type=int)
+    page['rows'] = max(page['rows'], 5)
+    page['rows_default'] = ROWS_PER_PAGE
+    page['num_pages'] = math.ceil(db.run.estimated_document_count() / page['rows'])
+    page['page'] = min(page['page'], page['num_pages'])
+
+    sort = {}
+    sort['by'] = request.args.get('sort', SORT_BY_DEFAULT)
+    sort['default'] = SORT_BY_DEFAULT
+    if sort['by'] not in ('runid', 'state', 'rcomment', 'simname', 'host', 'user'):
+        sort['by'] = SORT_BY_DEFAULT
+    sort['direction'] = request.args.get('direction', SORT_DIRECTION_DEFAULT, type=int)
+    sort['direction_default'] = SORT_DIRECTION_DEFAULT
+    if sort['direction'] not in (1, -1):
+        sort['direction'] = SORT_DIRECTION_DEFAULT
+
+    runs = db.run.find(skip=(page['page']-1)*page['rows'], limit=page['rows']).sort(sort['by'], sort['direction'])
+    return render_template("index.html", runs=runs, page=page, sort=sort)
 
 
 @bp.route("/<int:runid>")
