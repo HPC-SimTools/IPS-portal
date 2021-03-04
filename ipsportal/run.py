5bf33852d85d77eaf7eb45067d3a0e7a9e57d55f
Ross Whitfield <whitfieldre@ornl.gov>
2021-03-04 11:10:34 -0500

    Change id to runid

--- ipsportal/run.py
+++ ipsportal/run.py
@@ -16,16 +16,16 @@ def index():
     page = request.args.get('page', 1, type=int)
     num_pages = math.ceil(db.run.estimated_document_count() / ROWS_PER_PAGE)
     pages = range(1, num_pages+1)
-    runs = db.run.find(skip=(page-1)*ROWS_PER_PAGE, limit=ROWS_PER_PAGE).sort('id', pymongo.DESCENDING)
+    runs = db.run.find(skip=(page-1)*ROWS_PER_PAGE, limit=ROWS_PER_PAGE).sort('runid', pymongo.DESCENDING)
     return render_template("index.html", runs=runs, pages=pages, page=page, num_pages=num_pages)
 
 
-@bp.route("/<int:id>")
-def run(id):
+@bp.route("/<int:runid>")
+def run(runid):
     db = get_db()
-    r = db.run.find_one({'id': id})
+    r = db.run.find_one({'runid': runid})
     if r is None:
-        return render_template("notfound.html", run=id)
+        return render_template("notfound.html", run=runid)
 
     events = db.event.find({'portal_runid': r['portal_runid']}).sort('seqnum', pymongo.DESCENDING)
 
@@ -45,7 +45,7 @@ def event():
     db = get_db()
 
     if e.get('eventtype') == "IPS_START":
-        e['id'] = db.run.estimated_document_count()
+        e['runid'] = db.run.estimated_document_count()
         db.run.insert_one(e)
     elif e.get('eventtype') == "IPS_END":
         db.run.update_one({'portal_runid': e.get('portal_runid')}, {'$set': e})
