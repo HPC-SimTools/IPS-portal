443fa17b466db6a2ebe1faa6f71e6c358a236342
Ross Whitfield <whitfieldre@ornl.gov>
2022-05-20 16:44:57 -0400

    Test json from api

--- ips_test/run_test.sh
+++ ips_test/run_test.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
 export PYTHONPATH=$PWD
 ips.py --config=sim.conf --platform=platform.conf
-curl http://localhost/api/run/0
+
+newest_runid=$(curl http://localhost/api/runs | jq .[-1].runid)
+run_json=$(curl http://localhost/api/run/$newest_runid)
+
+# state
+if [ $(echo $run_json | jq .state) != '"Completed"' ]; then echo "state"; exit 1; fi
+# simname
+if [ $(echo $run_json | jq .simname) != '"simulation"' ]; then echo "simname"; exit 1; fi
+# rcomment
+if [ $(echo $run_json | jq .rcomment) != '"This-is-just-a-test"' ]; then
+    echo "rcomment";
+    exit 1;
+fi
+
+events=$(curl http://localhost/api/run/$newest_runid/events)
+
+# last event
+echo $events | jq .[-1]
