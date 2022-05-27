4baf72d2b0c94dfaf99db5d37e4e3e61898f1b1f
Ross Whitfield <whitfieldre@ornl.gov>
2022-05-27 14:43:04 -0400

    Fix monitor file url passing

--- ipsportal/analysis.py
+++ ipsportal/analysis.py
@@ -1,19 +1,19 @@
 import os
-from flask import Blueprint, session, redirect
+from flask import Blueprint, session, redirect, request
 import requests
 from ipsportal.login import login_required
 
 bp = Blueprint('analysis', __name__)
 
 
-@bp.route("/to_jupyter/<path:url>")
+@bp.route("/to_jupyter")
 @login_required
-def to_jupyter(url):
+def to_jupyter():
     filename = os.path.join(os.path.dirname(__file__), 'notebooks/monitor.ipynb')
     ipynb = open(filename).read()
     username = session['username']
     new_notebook = f"/global/homes/r/{username}/monitor.ipynb"
     requests.put("https://newt.nersc.gov/newt/file/cori/"+new_notebook,
-                 data=ipynb.replace("$URL", url),
+                 data=ipynb.replace("$URL", request.args.get('url')),
                  cookies={"newt_sessionid": session['newt_sessionid']})
     return redirect(f"https://jupyter.nersc.gov/user/{username}/cori-shared-node-cpu/lab/tree/{new_notebook}")
