c7a38f6977c8b9580e13e2469d78d571f748e170
Ross Whitfield <whitfieldre@ornl.gov>
2022-05-27 13:49:18 -0400

    Fix SECRET_KEY

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 # create random secret key
-python3 -c "import random,string;print('SECRET_KEY = \"'+''.join(random.choices(string.ascii_letters, k=64))+'\"')" > /usr/var/ipsportal-instance/config.py
+python -c 'import secrets; print(f"SECRET_KEY = \"{secrets.token_hex()}\"")' > /usr/local/var/ipsportal-instance/config.py
 
 exec "$@"
