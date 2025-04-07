795a0260aad14301dbe9abcf299aebf65b1e7f1c
Lance-Drane <Lance-Drane@users.noreply.github.com>
2025-04-07 17:55:51 -0400

    change jupyter API to use specific data files instead of python module
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

--- ipsportal/jupyter.py
+++ ipsportal/jupyter.py
@@ -4,10 +4,10 @@ from pathlib import Path
 
 from ._jupyter.hub_implementations import get_jupyter_url_prefix
 from ._jupyter.initializer import (
-    initialize_jupyter_import_module_file,
+    initialize_jupyter_data_files,
     initialize_jupyter_notebook,
     initialize_jupyter_python_api,
-    update_module_file_with_data_files,
+    update_data_listing_file,
     update_parent_module_file_with_child_runid,
 )
 from .db import get_parent_runid_by_child_runid
@@ -23,13 +23,11 @@ def _initialize_jupyterhub_dir(root_dir: Path, runid: int) -> bool:
     # TODO need to figure how to do authorization per-user
     try:
         os.makedirs(root_dir / 'data', exist_ok=True)
-        initialize_jupyter_import_module_file(root_dir, runid)
         initialize_jupyter_python_api(root_dir.parent)
+        initialize_jupyter_data_files(root_dir)
         parent_portal_runid = get_parent_runid_by_child_runid(runid)
         if parent_portal_runid:
-            update_parent_module_file_with_child_runid(
-                root_dir.parent / str(parent_portal_runid), parent_portal_runid, runid
-            )
+            update_parent_module_file_with_child_runid(root_dir.parent / str(parent_portal_runid), runid)
 
     except OSError as e:
         logger.warning(
@@ -46,7 +44,7 @@ def add_jupyter_notebook(runid: int, username: str, notebook_name: str, data: by
         return ('Server screwed up', 500)
 
     try:
-        initialize_jupyter_notebook(data, root_dir / notebook_name, runid)
+        initialize_jupyter_notebook(data, root_dir / notebook_name)
     except Exception:  # noqa: BLE001
         return ('Notebook was not valid', 400)
     # Return the fully qualified URL
@@ -70,7 +68,7 @@ def add_analysis_data_file_for_timestep(
         f.write(data)
 
     try:
-        update_module_file_with_data_files(root_dir, runid, filename, replace, timestamp)
+        update_data_listing_file(root_dir, filename, timestamp)
     except Exception:
         logger.exception('Unable to update module file with the data files')
         return ("Server couldn't update module file", 500)
